85ab130080bf8766d9cee7109300dccbd3d6bf6e
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2024-11-18 14:50:04 +0700

    Thêm class

---
+++ Main.cs
@@ -0,0 +1,21 @@
+﻿using QuanLiNhanSu_YT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Class
+{
+    internal class main
+    {
+        public static void Main(string[] args)
+        {
+            Student student = new Student("0792","Hoten",20,"30/07/2005","231102","9");
+            student.enrollCourse("Li","Nam");
+            //student.addScore("Li", "Nam", 10);
+            MessageBox.Show($"{student.ToString()}","Thong tin", MessageBoxButtons.OKCancel,MessageBoxIcon.Information);
+        }
+    }
+}
